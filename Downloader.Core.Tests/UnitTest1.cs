67ae710d8d16eeb5d2fd9b20cdcf2c4cd741cc4f
VoThanhNhan <nvo682052@gmail.com>
2026-08-26 12:48:53 +0700

    cấu trưc cơ bản

---
+++ Downloader.Core.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Downloader.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Downloader.IO.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Downloader.IO.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
